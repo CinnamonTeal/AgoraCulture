52cbc72151921bbb8579bd25cee705e0c8dc0bd3
manicalshawn <hansenpshawn@gmail.com>
2015-04-06 14:36:26 -0400

    add comments.

--- app/datafetchers/OpenDataApiCaller.rb
+++ app/datafetchers/OpenDataApiCaller.rb
@@ -4,4 +4,7 @@ class OpenDataApiCaller
    @market_data = JSON.load(open("https://data.cityofnewyork.us/resource/j8gx-kc43.json"))
  end
 
+ # market_data[0].keys => ["facilityaddinfo", "facilitystate", "facilityname", "longitude", "latitude", "facilitystreetname", "facilitycity", "facilityzipcode"] 
+ # market_data[0]['facilityname'] => "Brooklyn Borough Hall Greenmarket" 
+
 end
