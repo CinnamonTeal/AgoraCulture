7610774a5e913938e1a200571a2cf1b7f83b3a95
manicalshawn <hansenpshawn@gmail.com>
2015-04-07 15:41:36 -0400

    stash.

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,18 +1,17 @@
 function initialize() {
 
-      var FlatironBrooklyn = new google.maps.LatLng(40.697325, -73.986014);
+  var FlatironBrooklyn = new google.maps.LatLng(40.697325, -73.986014);
 
-      var mapOptions = {
-        center: FlatironBrooklyn,
-        zoom: 14
-      };
-      var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+  var mapOptions = {
+    center: FlatironBrooklyn,
+    zoom: 14
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
 
-      var marker = new google.maps.Marker({
-        position: FlatironBrooklyn,
-        map: map,
-        title: 'Flatiron Brooklyn'
-      })
-
-      }
+  var marker = new google.maps.Marker({
+    position: FlatironBrooklyn,
+    map: map,
+    title: 'Flatiron Brooklyn'
+  })
+}
