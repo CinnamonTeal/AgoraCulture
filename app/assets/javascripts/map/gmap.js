d01f08bc064aeefbc607669cbb9f23bca5094d3d
CinnamonTeal <happythunder.lionheart@gmail.com>
2015-04-07 20:46:57 +0200

    moved map javascript to /javascripts/map/gmap.js

---
+++ app/assets/javascripts/map/gmap.js
@@ -0,0 +1,18 @@
+function initialize() {
+
+      var FlatironBrooklyn = new google.maps.LatLng(40.697325, -73.986014);
+
+      var mapOptions = {
+        center: FlatironBrooklyn,
+        zoom: 14
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: FlatironBrooklyn,
+        map: map,
+        title: 'Flatiron Brooklyn'
+      })
+
+      }
