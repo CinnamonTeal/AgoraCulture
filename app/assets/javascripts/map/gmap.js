f8b8a7be2087d5c2d3aeab6a85ace900e8d05b47
manicalshawn <hansenpshawn@gmail.com>
2015-04-08 15:39:45 -0400

    markers added.

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,7 +1,5 @@
 var geocoder;
 var map;
-
-
 function initialize() {
   geocoder = new google.maps.Geocoder();
   var latlng = new google.maps.LatLng(40.697325, -73.986014);
@@ -12,31 +10,13 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-// function codeAddress() {
-//   var address = document.getElementById('address').value;
-//   geocoder.geocode( { 'address': address}, function(results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//       map.setCenter(results[0].geometry.location);
-//       var marker = new google.maps.Marker({
-//           map: map,
-//           position: results[0].geometry.location
-//       });
-//     } else {
-//       alert('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-// }
-
 function addPin(market) {
   var lat = market[1];
   var lon = market[2];
+  var marketLatlng = new google.maps.LatLng(market[1], market[2]);
 
-  // add pins here
-  
-  var marketlatlng = new google.maps.LatLng(lat, lon);
-
-  new google.maps.Marker({
-        position: marketlatlng,
-        map: map
-      });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: marketLatlng
+  });
 }
