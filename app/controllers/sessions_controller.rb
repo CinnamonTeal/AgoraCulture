76691eab6869aeb7eda5ba691a1bf3474a205161
manicalshawn <hansenpshawn@gmail.com>
2015-04-09 15:55:13 -0400

    google omni_auth & market show complete.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || User.create_with_omniauth(auth)
 		session[:user_id] = user.id
-		redirect_to "/user/#{user.id}", flash[:notice] => "Signed in!"
+		redirect_to "/user/#{user.id}"
 	end
 
 	def destroy
 		reset_session
-		redirect_to "/", :notice => "Signed out!"
+		redirect_to "/"
 	end
 
 	private
 	def auth
-		request.env[:omniauth.auth]
+		request.env["omniauth.auth"]
 	end
 
 end
