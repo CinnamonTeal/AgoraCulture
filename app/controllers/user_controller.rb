7fb220287b42177d8a9a075dcf5664ade4d5308b
manicalshawn <hansenpshawn@gmail.com>
2015-04-09 10:28:40 -0400

    broken auth.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,12 @@ class UserController < ApplicationController
 	def show
 		@user = User.find_by_id(params[:id])
 	end
+
+	private
+
+def current_user
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
+helper_method :current_user
+
 end
