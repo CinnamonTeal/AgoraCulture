e83f4870be5c70053ebdd74934aced0722781aa5
CinnamonTeal <happythunder.lionheart@gmail.com>
2015-04-10 14:22:28 +0200

    add route

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,6 +1,6 @@
 class ReviewController < ApplicationController
   def new
     Review.create(title: params[:title], rating: params[:rating], user_id: params[:id], market_id: params[:market_id])
+    redirect_to 'market/#{params[:market_id]}'
   end
-
 end
